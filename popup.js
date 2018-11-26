38611f2ce4119ba9a121335d0c9155a555e1987c
ImperiumB <pahmaks@gmail.com>
2018-11-26 16:44:29 +0300

    Branch first changes

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 var createPassword = function() {
-  var passAt = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  var passArray = Array.from({length: 15})
+var L==15 
+var passAt = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  var passArray = Array.from({length: L})
 
   return passArray.map(function(_, index) { 
     return index % 4 == 3 ? '-' : passAt.charAt(Math.random() * passAt.length)
